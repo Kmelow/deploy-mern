9831185742008b8d6b0021509300d74936372ad4
Kmelow <camilo.c@protonmail.com>
2019-03-13 11:08:29 +0100

    Fixing deployment issue

--- index.js
+++ index.js
@@ -2,15 +2,8 @@
 if (process.env.NODE_ENV === 'production') {
 	// Express will serve up production assets
   // like main.js or main.css
-	app.use(express.static('./dist'));
-	// Express will serve up index.html file
-  // if it doesn't recognize the route
-	const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(
-			__dirname, 'dist', 'index.html'
-		))
-	})
+	require('./dist/index_bundle.js');
+	// app.use(express.static('./dist'));
 }
 
 require('./src/server/server')
