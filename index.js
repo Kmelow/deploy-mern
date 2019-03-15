0ed08e1203ccb462076753dd5169dfcc9ece536f
Kmelow <camilo.c@protonmail.com>
2019-03-15 17:52:47 +0100

    Fixing deployment issue

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
+const path = require('path');
 
-// Requiring Mongoose Schemas
-require('./models/Item');
-// Mapping global promisses
-mongoose.Promise = global.Promise;
+// Requiring routes
+const item = require('./src/server/routes/api/item');
 
-// Requiring Routes
-const items = require('./routes/api/items');
-
-// Loading Routes
-
-// Launch Express
+// Start server
 const app = express();
 
-// BodyParser Middleware
-app.use(bodyParser.json());
+// Load routes
+app.use('/api/item', item);
 
-//Connect to Mongose
-mongoose
-	.connect(keys.mongoURI, { useNewUrlParser: true })
-	.then(console.log('MongoDB connected...'))
-	.catch((err) => console.log(err));
+// Home page
+app.use(express.static('dist'));
 
-// Use Routes
-app.use('/api/items', items);
-app.use('/', (req, res) => res.send('Hellooooo'));
-
-// Set up for production
-if (process.env.NODE_ENV === 'production') {
-	// Express will serve up production assets
-	// like main.js or main.css
-	app.use(express.static('dist'));
-	// Express will serve up index.html file
-	// if it doesn't recognize the route
-	const path = require('path');
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-	});
-}
-
-// Listen on port 5000 if not set by environment
+// Define port
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/client/index.js',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: "index_bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
+	entry: './src/client/index.js',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'WP_bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
 			},
 			{
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			}
+		]
 	},
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/api": "http://localhost:5000"
-    }
-  },
-  plugins: [
-		new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-			title: "Output management",
-      template: "./src/client/public/index.html",
-      favicon: "./src/client/public/favicon.ico"
-    })
-  ]
+	devServer: {
+		port: 3000,
+		open: true,
+		proxy: {
+			'/api': 'http://localhost:5000'
+		}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/public/index.html'
+		})
+	]
 };
